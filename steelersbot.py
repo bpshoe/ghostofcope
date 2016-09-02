bb8fd2b60aa55f485c1838ed556d50a1fcfd44b0
Brian Shoemaker <bpshoe@gmail.com>
2016-09-01 21:30:50 -0400

    added read python file

---
+++ steelersbot.py
@@ -0,0 +1,50 @@
+import praw
+
+user_agent = ("SteelersBot v0.0")
+
+
+r = praw.Reddit(user_agent = user_agent)
+
+subreddit = r.get_subreddit("steelers")
+
+for submission in subreddit.get_hot(limit = 5):
+
+
+
+ dir(submission)
+ 
+['approve',
+'approved_by',
+'author',
+ 
+'domain',
+'downs',
+'downvote',
+'edit',
+'edited',
+ 
+'saved',
+ 
+'score',
+'secure_media',
+'secure_media_embed',
+'selftext',
+'selftext_html',
+ 
+'title',
+ 
+'ups',
+'upvote',
+'url',
+'user_reports',
+'visited',
+'vote']
+
+
+
+
+for submission in subreddit.get_hot(limit = 5):
+    print "Title: ", submission.title
+    print "Text: ", submission.selftext
+    print "Score: ", submission.score
+    print "---------------------------------\n"
